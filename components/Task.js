a2280a6d46cbd6278089a50baa4d6c59153aee27
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-27 16:29:49 +0530

    Simple Task Component Created

---
+++ components/Task.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+import React from 'react';
+
+const Task = () => {
+    return (
+        <View>
+            <Text>This is a Task</Text>
+        </View>
+    );
+};
+
+export default Task;
